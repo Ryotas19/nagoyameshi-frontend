0c2e7b77f4a09b072193ab6054863a3ceeb63537
Ryotas19 <ryotassoccer19@gmail.com>
2025-06-30 13:32:23 +0900

    Delete api directory

--- api/admin.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from .models import Restaurant, Review, Category, Favorite, Reservation
-
-admin.site.register(Restaurant)
-admin.site.register(Review)
-admin.site.register(Category)
-admin.site.register(Favorite)
-admin.site.register(Reservation)
